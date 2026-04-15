8992302b9347e34266fe44dc96b1250abc7ae0fd
muhumuza684 <andrewbright256@gmail.com>
2026-04-15 20:02:06 +0300

    Fix syntax error: remove trailing comma

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template_string, redirect, url_for, flash, request, session, jsonify,
+from flask import Flask, render_template_string, redirect, url_for, flash, request, session, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
